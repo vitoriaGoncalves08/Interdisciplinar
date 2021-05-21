787c821c93f154a70efd7d28eccb4f93fa23e0ef
Vitória Gonçalves Passos <vitoriaGoncalves08@users.noreply.github.com>
2021-05-21 20:45:04 -0300

    link inserir vitorinha

--- index.php
+++ index.php
@@ -1,68 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projeto Interdiciplinar</title>
-</head>
-
-<body>
-    <style>
-    body {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-    }
-
-    form {
-        display: flex;
-        width: 50%;
-        padding: 30px;
-        background-color: #CCC;
-        flex-direction: column;
-    }
-
-    form input {
-        padding: 10px;
-        font-size: 1.2em;
-        margin: 10px 0;
-    }
-
-    input[type='submit'] {
-        cursor: pointer;
-        background-color: rgb(120, 120, 120);
-    }
-    
-    </style>
-    <form action="./Controllers/InserirPalavra.php" id='form' method="POST">
-        <input type="text" placeholder="Palavra" id="palavra" required>
-        <input type="text" placeholder="Traducao" id="traducao" required>
-        <input type="text" placeholder="Descricao" id="descricao" required>
-        <input type="submit" value="Inserir">
-    </form>
-
-</body>
-<script>
-    let form = document.querySelector('#form');
-    let palavra = document.querySelector('#palavra');
-    let traducao = document.querySelector('#traducao');
-    let descricao = document.querySelector('#descricao');
-    form.addEventListener('submit', (evt)=>{
-        evt.preventDefault();
-        async function Go(){
-            let req = await fetch(`./Controllers/InserirPalavra.php?
-            palavra=${palavra.value}&traducao=${traducao.value}&descricao=${descricao.value}`)
-            let res = await req.text();
-            alert(res);
-        }
-        Go();
-        palavra.value = '';
-        traducao.value = '';
-        descricao.value = '';
-
-})
-</script>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>dbInterdisciplinar</title>
+    </head>
+    <body>
+        <form method="POST" action="">
+            <label>Assunto: </label>
+            <input type="text" list="datas" name="termo" id="termo"  placeholder="Pesquisar termo">
+            <datalist id="datas">
+            </datalist>
+            <input type="button" id="btn" name="PesqTermo" value="Pesquisar">
+            <a href="./Public//View/index_inserir.php">Inserir</a>
+        </form><br><br>
+        <div id='content'>
+        </div>
+        <script>
+            'use strict';
+            let termo = document.querySelector('#termo');
+            termo.addEventListener('change',async(evt)=>{
+                evt.preventDefault();
+                let req = await fetch(`./Controllers/PegaPalavra.php?term=${termo.value}`);
+                let res = JSON.parse(await req.json());
+                let content = document.querySelector('#content');
+               for (let i = 0; i < res.length; i++) {
+                    content.innerHTML += 
+                    `<p>Nome: ${res[i].nomePalavra}</p>
+                    <p>Traducao: ${res[i].traducaoPalavra}</p>
+                    <p>Descricao: ${res[i].descricaoPalavra}</p>
+                    <hr>
+                    `               ;
+                    document.querySelector('#datas').innerHTML += `<option>${res[i].nomePalavra}</option>` 
+               }
+            })
+        </script>
+    </body>
 </html>
