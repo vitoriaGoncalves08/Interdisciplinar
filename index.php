8b9d338990738c32e00a25a03c67f87134c2f6ee
gDelps <guilhermedelfino25@gmail.com>
2021-05-24 21:14:34 -0300

    Commit delfino ajudou vitorinha com toggle menu 24-5

--- index.php
+++ index.php
@@ -3,16 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <title>Sterms</title>
-
         <link rel="stylesheet" href="./Public/assets/css/style.css"/>
-        <script src="./Public/assets/javascript/jquery.js"></script>
     </head>
     <body>
 
     <header id="header">
         <a id="logo" href="">STerms</a>
         <nav id="nav">
-            <button class="btn-mobile">Menu</button>
+            <button id="btn-mobile">Menu</button>
             <ul id="menu">
                 <li><a href="./Public/View/index_inserir.php">Insert</a></li>
                 <li><a href="#">Home</a></li>
@@ -29,6 +27,7 @@
         </form><br><br>
         <div id='content'>
         </div>
+        <script src="./Public/assets/javascript/jquery.js"></script>
         <script>
             'use strict';
             let termo = document.querySelector('#termo');
@@ -37,8 +36,8 @@
             req.then(el=>{
                 el.json().then(el=>{
                     for (let i = 0; i < el.length; i++) {
-                    document.querySelector('#datas').innerHTML += `<option>${el[i].nomePalavra}</option>` 
-                }
+                        document.querySelector('#datas').innerHTML += `<option>${el[i].nomePalavra}</option>` 
+                    }
                 })
             })
             termo.addEventListener('change',async(evt)=>{
@@ -58,8 +57,5 @@
                 }
             })
         </script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
     </body>
 </html>
